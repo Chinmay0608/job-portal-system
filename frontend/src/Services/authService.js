9bdb6e555923fecc466765c4c6efd926205c2342
chinmay <chinmay8064@gmail.com>
2026-06-12 18:22:43 +0530

    Fix auth API production

--- frontend/src/Services/authService.js
+++ frontend/src/Services/authService.js
@@ -1,16 +1,40 @@
 import axios from "axios";
 
-/* Backend URL */
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
+const API_URL =
+  `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
 
-/* Register User */
-export const registerUser = async (userData) => {
-  const response = await axios.post(`${API_URL}/register`, userData);
-  return response.data;
-};
+export const registerUser =
+  async (userData) => {
 
-/* Login User */
-export const loginUser = async (userData) => {
-  const response = await axios.post(`${API_URL}/login`, userData);
-  return response.data;
-};
+    const response =
+      await axios.post(
+        `${API_URL}/register`,
+        userData,
+        {
+          headers: {
+            "Content-Type":
+              "application/json",
+          },
+        }
+      );
+
+    return response.data;
+  };
+
+export const loginUser =
+  async (userData) => {
+
+    const response =
+      await axios.post(
+        `${API_URL}/login`,
+        userData,
+        {
+          headers: {
+            "Content-Type":
+              "application/json",
+          },
+        }
+      );
+
+    return response.data;
+  };
